a4ea8c26ae2a0a23600f23946cfec450e1751841
rghostin <rawadghostin@protonmail.com>
2020-11-23 21:59:00 +0200

    file upload

--- frontend/static/frontend/assets/javascript/create.js
+++ frontend/static/frontend/assets/javascript/create.js
@@ -1,13 +1,20 @@
 function create_intel(api_endpoint, data) {
-    console.log(data);
-
     $.ajax({
         type: "POST",
         url: api_endpoint,
         data: data,
         success: function(data) {
-               console.log(0);
-               console.log(data);
-            }
+           console.log(0);
+           console.log(data);
+           console.log(data.id);
+           // upload_files(data.id);
+        }
     });
+}
+
+function add_file_to_table(file) {
+    console.log(file);
+     $("#id_files_table tbody").prepend(
+        "<tr><td>"+file.name+"</td><td>"+file.type+"</td><td>"+file.size+"</td></tr>"
+    )
 }
