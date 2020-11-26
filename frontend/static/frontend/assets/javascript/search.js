af007c9d3165ba828a27f29eba48e13f871fb735
rghostin <rawadghostin@protonmail.com>
2020-11-26 21:28:05 +0200

    page in load

--- frontend/static/frontend/assets/javascript/search.js
+++ frontend/static/frontend/assets/javascript/search.js
@@ -1,31 +1,46 @@
-function ajax_load_filtered_intels(api_endpoint, detail_view_endpoint, filter) {
+function ajax_load_filtered_intels(api_endpoint, detail_view_endpoint, filterArray, page) {
     let show_results_banner = false;
-    if (filter != null) {
+    if (filterArray) {
         show_results_banner = true;
     }
+
+    let data = [];
+    if (filterArray) {
+        data = data.concat(filterArray)
+    }
+    if (page) {
+        data.push({name: "page", value: page})
+    }
+    console.log(data);
+
+
     $.ajax({
         url: api_endpoint,
         contentType: "application/json",
         dataType: 'json',
-        data: filter,
+        data: $.param(data),
         beforeSend: function(jqXHR, settings) {console.log(settings.url);},
         success: function(data){
-            $('#id_search_results_body').empty();
-            for (let i = 0; i < data.count; i++) {
-                let entry = data.results[i];
-                append_search_entry(entry, detail_view_endpoint)
-            }
+            // $('#id_search_results_body').empty();
+            // for (let i = 0; i < data.count; i++) {
+            //     let entry = data.results[i];
+            //     append_search_entry(entry, detail_view_endpoint)
+            // }
 
             if (show_results_banner) {
                 display_results_banner(data.count);
             }
+
+            set_page_num(page);
+
+
         }
         // todo error
     });
 }
 
 function ajax_load_all_resources(api_endpoint, detail_view_endpoint) {
-    ajax_load_filtered_intels(api_endpoint, detail_view_endpoint, null);
+    ajax_load_filtered_intels(api_endpoint, detail_view_endpoint, null, null);
     hide_results_banner()
 }
 
@@ -65,4 +80,12 @@ function display_results_banner(number) {
 
 function hide_results_banner() {
     $('#id_results_banner').hide();
+}
+
+function set_page_num(num) {
+    console.log(num);
+    if (!num){
+        num=1;
+    }
+    $("#id_page_num").text(num.toString());
 }
