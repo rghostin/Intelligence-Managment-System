ff33dd124f9696f24727fa2cb81a8c41b9a87202
rghostin <rawadghostin@protonmail.com>
2020-12-20 21:00:28 +0200

    init tags script

---
+++ scripts/init_tags.py
@@ -0,0 +1,83 @@
+from intelsAPI.models import Tag
+
+TAGS = {
+    # network
+    'network',
+    'network-protocol',
+    'network-scanning',
+
+    # protocols
+    'dns',
+    'finger',
+    'ftp',
+    'http',
+    'ipsec',
+    'ipv6',
+    'kerberos',
+    'nfs',
+    'oauth',
+    'smtp',
+    'snmp',
+    'ssh',
+    'tcp',
+    'tls',
+    'tor',
+
+    # web
+    'web-security',
+    'csrf',
+    'ssrf',
+    'sqli',
+    'xss',
+    'xxe',
+
+    # wireless
+    'wireless',
+    'wifi',
+    'wep',
+    'wpa',
+
+    # anonymity
+    'anonymity',
+    'covering-tracks',
+    'secure-erase',
+
+    # cryptography
+    'cryptography',
+    'ntlm',
+
+    # exploit development
+    'exploit-development',
+    'buffer-oveflow',
+
+    # secure development
+    'secure-development',
+
+    # pentesting concepts
+    'bruteforce',
+    'pivoting',
+    'privesc',
+    'user-enumeration',
+
+    # operating systems
+    'linux',
+    'windows',
+    'mac',
+
+    # malware
+    'malware',
+
+    # forensics
+    'forensics',
+
+    # other
+    'guide',
+}
+
+
+def run(*args):
+    print("[*] Initializing tags (%d)" % len(TAGS))
+    for tagname in TAGS:
+        print(f"\t- {tagname}")
+        Tag.objects.create(name=tagname)
+
