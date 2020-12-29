550ea6501b334a0a8be08006dd866c5b5dd933b5
rghostin <rawadghostin@protonmail.com>
2020-12-30 00:10:56 +0200

    script run_backup.sh remote sync

--- scripts/run_backup.sh
+++ scripts/run_backup.sh
@@ -5,19 +5,9 @@
 #   MAILTO = "sharp.imsystem@gmail.com"
 #   0 4,18 * * * <PROJ_ROOT_DIR>/scripts/run_backup.sh > /tmp/sharp_backup.log
 
-# cloud sync
-# rclone sync <local_bkp_dir> "<drive>:<remote_bkp_dir"
-
 
 set -e
 
-
-function assert_installed {
-  if ! [ -x "$(command -v "$1")" ]; then
-    echo "Error: $1 not found" >&2
-    exit 1
-  fi
-}
 
 # TO CONFIGURE - set absolute path to project
 PROJ_ROOT_DIR="$(pwd)"
@@ -28,18 +18,29 @@ LOCAL_BACKUP_DIR="/tmp/testdbbackup/"
 REMOTE_BACKUP_DIR="drive:sharp_backup"
 
 
+function assert_installed {
+  if ! [ -x "$(command -v "$1")" ]; then
+    echo "Error: $1 not found" >&2
+    exit 1
+  fi
+}
+
+
+function log {
+  echo "[*] $(date) - $1"
+}
+
 assert_installed "${RCLONE}"
 
-
 echo '================================================'
-echo "[*] $(date) - Starting local backup"
+log "Starting local backup"
 
 source "$VENV_ACTIVATE"
 python3 "${MANAGE_SCRIPT}" dbbackup --clean --compress
 python3 "${MANAGE_SCRIPT}" mediabackup --clean --compress
 deactivate
 
-echo "[*] $(date) - Starting remote syncing"
+log "Starting remote syncing"
 "${RCLONE}" sync "${LOCAL_BACKUP_DIR}" "${REMOTE_BACKUP_DIR}"
 
-echo "[*] $(date) - Backup completed"
+log "Backup completed"
