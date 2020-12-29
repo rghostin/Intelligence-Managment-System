9dfcbaf0a404774ed8f274ede8833d8d6a5b7c4a
rghostin <rawadghostin@protonmail.com>
2020-12-29 23:48:23 +0200

    script run_backup.sh

--- scripts/run_backup.sh
+++ scripts/run_backup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Usage :
+# e.g run everyday at 4AM and 6PM : crontab -e
+#   MAILTO = "sharp.imsystem@gmail.com"
+#   0 4,18 * * * <PROJ_ROOT>/scripts/run_backup.sh > /tmp/sharp_backup.log
+
 set -e
 
 # set absolute path to project
-DJ_ROOT_DIR="$(pwd)"
-VENV_ACTIVATE="$DJ_ROOT_DIR/venv/bin/activate"
+PROJ_ROOT_DIR="$(pwd)"
+VENV_ACTIVATE="${PROJ_ROOT_DIR}/venv/bin/activate"
+MANAGE_SCRIPT="${PROJ_ROOT_DIR}/manage.py"
 
 line="================================================"
 
@@ -12,8 +18,8 @@ echo "$line"
 echo "$(date) - Starting backup"
 
 source "$VENV_ACTIVATE"
-python3 "$DJ_ROOT_DIR/manage.py" dbbackup --clean --compress
-python3 "$DJ_ROOT_DIR/manage.py" mediabackup --clean --compress
+python3 "${MANAGE_SCRIPT}" dbbackup --clean --compress
+python3 "${MANAGE_SCRIPT}" mediabackup --clean --compress
 deactivate
 
 echo "$(date) - Backup completed"
